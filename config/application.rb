cfb21cb0f7e75cd23668c54715f38a5eb6261ba5
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-04-02 16:09:59 +0300

    changed application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Mysmilesapi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.credentials = Rails.application.credentials
+    config.require_master_key = true
+
 
 
     # Configuration for the application, engines, and railties goes here.

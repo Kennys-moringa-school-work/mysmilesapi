a22198768000b9d524e11e896a170a512b8432b1
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-04-02 15:22:41 +0300

    trial 1

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Mysmilesapi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.credentials = Rails.application.credentials
+
 
     # Configuration for the application, engines, and railties goes here.
     #
